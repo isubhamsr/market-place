67fc432959d8a05451a0604384fd7a91e8d8dbd4
Subham Roy <52313227+isubhamsr@users.noreply.github.com>
2020-08-12 14:18:55 +0530

    first commit

---
+++ insta-api/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const PORT = 3001
+
+app.get("/", (req,res)=>{
+    res.send("hello")
+})
+
+app.listen(PORT, ()=> console.log("Running on ",PORT))
