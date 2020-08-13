9f0d6447f54364e15db46f6cd1b496c27b06b13f
Subham Roy <52313227+isubhamsr@users.noreply.github.com>
2020-08-13 15:04:10 +0530

    add username field

--- insta-api/controllers/auth.js
+++ insta-api/controllers/auth.js
@@ -8,34 +8,45 @@ let auth = {}
 
 auth.signup = (req, res) => {
     try {
-        const { name, email, password } = req.body
-        if (!name || !email || !password) {
+        const { name, username, email, password } = req.body
+        if (!name || !username || !email || !password) {
             return res.status(422).json({ error: true, msg: "All Fields are require" })
         }
         User.findOne({ email: email })
-        .then((saveUser) => { 
-            if(saveUser){
-                return res.status(422).json({ error: true, msg: "This email is allready taken" })
-            }
-            const salt = bcrypt.genSaltSync(parseInt(process.env.SALT_Rounds));
-            const hash = bcrypt.hashSync(password, salt);
-            const user = new User({
-                name: name,
-                email : email,
-                password : hash
-            })
+            .then((emailId) => {
+                if (emailId) {
+                    return res.status(422).json({ error: true, msg: "This email is allready taken" })
+                }
+                User.findOne({ username: username })
+                    .then((userName) => {
+                        if (userName) {
+                            return res.status(422).json({ error: true, msg: "This Username is allready taken" })
+                        }
 
-            user.save()
-            .then((user)=>{
-                return res.status(200).json({ error: false, msg: "Signup Success", data: user })
+                        const salt = bcrypt.genSaltSync(parseInt(process.env.SALT_Rounds));
+                        const hash = bcrypt.hashSync(password, salt);
+                        const user = new User({
+                            name : name,
+                            username : username,
+                            email : email,
+                            password : hash
+                        })
+
+                        user.save()
+                            .then((user) => {
+                                return res.status(200).json({ error: false, msg: "Signup Success", data: user })
+                            })
+                            .catch((error) => {
+                                return res.status(422).json({ error: true, msg: error.message })
+                            })
+                    })
+                    .catch((error) => {
+                        res.status(500).json({ error: true, msg: error.message })
+                    })
             })
-            .catch((error)=>{
-                return res.status(422).json({ error: true, msg: error.message })
+            .catch((error) => {
+                res.status(500).json({ error: true, msg: error.message })
             })
-        })
-        .catch((error)=>{
-            res.status(500).json({ error: true, msg: error.message })
-        })
 
     } catch (error) {
         res.status(500).json({
