4ee454b8be4982b55f00dd22c7e802c16fce46a6
Subham Roy <52313227+isubhamsr@users.noreply.github.com>
2020-08-13 14:49:27 +0530

    add user to database

--- insta-api/controllers/auth.js
+++ insta-api/controllers/auth.js
@@ -1,9 +1,42 @@
+const mongoose = require("mongoose")
+const bcrypt = require('bcrypt');
+const dotenv = require("dotenv")
+dotenv.config()
+const User = mongoose.model("User")
+
 let auth = {}
 
 auth.signup = (req, res) => {
     try {
+        const { name, email, password } = req.body
+        if (!name || !email || !password) {
+            return res.status(422).json({ error: true, msg: "All Fields are require" })
+        }
+        User.findOne({ email: email })
+        .then((saveUser) => { 
+            if(saveUser){
+                return res.status(422).json({ error: true, msg: "This email is allready taken" })
+            }
+            const salt = bcrypt.genSaltSync(parseInt(process.env.SALT_Rounds));
+            const hash = bcrypt.hashSync(password, salt);
+            const user = new User({
+                name: name,
+                email : email,
+                password : hash
+            })
 
-        res.json({ msg: "sign up route" })
+            user.save()
+            .then((user)=>{
+                return res.status(200).json({ error: false, msg: "Signup Success", data: user })
+            })
+            .catch((error)=>{
+                return res.status(422).json({ error: true, msg: error.message })
+            })
+        })
+        .catch((error)=>{
+            res.status(500).json({ error: true, msg: error.message })
+        })
+
     } catch (error) {
         res.status(500).json({
             err: true,
--- insta-api/index.js
+++ insta-api/index.js
@@ -12,7 +12,8 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// qLcr83GSS8HsaYwr
+// register the model schema
+require("./model/userModel")
 
 // import routes
 const apiRoute = require("./routes/api")
